840e5dd970981785036e12f088351000193f1db6
Guru <guru85111@gmail.com>
2025-01-04 17:16:22 +0530

    `feat: Added react-icons package and implemented basic todo list functionality`

--- todo-list-react/src/App.js
+++ todo-list-react/src/App.js
@@ -1,7 +1,37 @@
+import {useState} from 'react'
+import { MdDeleteOutline } from "react-icons/md";
 
 function App() {
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTasks(prev => [...prev, newTask])
+    console.log(tasks);
+    
+  }
+  
+  const handleDelete = (task) => {
+    
+    setTasks(prev => prev.filter(item => item!==task))
+  }
   return (
-      <div className='text-blue-400'>Hello</div>
+      <section className=''>
+        <div>
+          <form>
+            <input type="search" name='task' placeholder='Add task' onChange={handleChange} />
+            <button onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+{tasks.map((task, index) => (
+  <div key={index}>{task} <MdDeleteOutline onClick={() => handleDelete(task)} /></div>
+))}
+      </section>
   );
 }
 
