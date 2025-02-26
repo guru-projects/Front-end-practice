f1274156494010a2ad5442682d6c28f62a1d5db2
Guru <guru85111@gmail.com>
2025-02-26 20:17:57 +0530

    updated tailwindcss config

--- ip-tracker/tailwind.config.js
+++ ip-tracker/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx}","./index.html"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}" // Added ts and tsx for TypeScript support
+  ],
   theme: {
+    screens: {
+	sm: '480px',
+	md: '768px',
+	lg: '976px',
+	xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
 }
-
